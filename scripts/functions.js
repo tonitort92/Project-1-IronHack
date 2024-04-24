75b46ade4a11c0008bb05e63d9f856f2be832ed0
Toni <toni.tort92@gmail.com>
2024-04-24 18:05:55 +0200

    DAY 3 | Responsive ready, form ready, fetch ready, toogle ready

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,3 +1,52 @@
+async function retrieveBackData() {
+    try {
+        console.log("Fetching data...");
+        const response = await fetch("https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects");
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const backData = await response.json(); // Convertir la respuesta a JSON aquí
+        console.log(backData);
+        if (backData) {
+            let vectorifyProject = backData[1];
+            let dashcoinProject = backData[2];
+            let simplifyProject = backData[3];
+
+            document.querySelector('#recent-projects h3').textContent = simplifyProject.name;
+            document.querySelector('#recent-projects p').textContent = simplifyProject.description;
+            document.querySelector('.project-card div:nth-of-type(1)').style.backgroundImage = 'url(' + simplifyProject.image + ')';
+        
+            document.querySelectorAll('#recent-projects h3')[1].textContent = dashcoinProject.name;
+            document.querySelectorAll('#recent-projects p')[1].textContent = dashcoinProject.description;
+            document.querySelectorAll('.project-card div:nth-of-type(1)')[1].style.backgroundImage = 'url(' + dashcoinProject.image + ')';
+        
+            document.querySelectorAll('#recent-projects h3')[2].textContent = vectorifyProject.name;
+            document.querySelectorAll('#recent-projects p')[2].textContent = vectorifyProject.description;
+            document.querySelectorAll('.project-card div:nth-of-type(1)')[2].style.backgroundImage = 'url(' + vectorifyProject.image + ')';
+        
+            document.querySelector("#main-project h1").textContent = backData[1].name;
+            document.querySelector("#main-project .date p").textContent = backData[1].description;
+            document.querySelector("#main-project .date span").textContent = backData[1].completed_on;
+            document.querySelector("#main-project .img-project").setAttribute("src", backData[0].image);
+            document.querySelector("#main-project .text-project p").textContent = backData[1].content;
+
+        }
+    } catch (error) {
+        console.log("Something went wrong!", error.message);
+    }
+}
+
+retrieveBackData();
+
+/*function projectsSubstitution (a, b, c, d){
+
+    document.querySelectorAll('#recent-projects-grid .project-card div h3')[0].textContent = b.
+
+}
+
+projectsSubstitution( mainProject, vectorifyProject, dashcoinProject, simplifyProject);*/
+
+
 window.addEventListener('load', () => {
 
     //We want to fix a bug with the mobile menu, as it keeps open in desktop if we open it in mobile //
@@ -27,6 +76,26 @@ window.addEventListener('load', () => {
         }
     });
 
+    //Validating the form//
+
+    document.querySelector('#contact-form').addEventListener('submit', function(event) {
+        event.preventDefault(); // Esto evita que el formulario se envíe automáticamente.
+    
+        let mainName = document.querySelector('input[type=text]').value;
+        let mail = document.querySelector('input[type=email]').value;
+        let phone = document.querySelector('input[type=tel]').value;
+        let message = document.querySelector('textarea').value; 
+    
+        if(mainName.length == 0 || mail.length == 0 || phone.length == 0 || message.length == 0){
+            alert('Formulario incompleto, vacío o incorrecto');
+        }
+        else{
+            alert('Se ha mandado tu mensaje :D');
+            // Aquí puedes agregar código para enviar los datos del formulario de manera programática si así lo deseas.
+        }
+    });
+
+
     //Now, if you open the toogle, but you resize to desktop, we hide it. If you go back to mobile, we show it again //
 
     window.addEventListener('resize', () => {
@@ -45,6 +114,7 @@ window.addEventListener('load', () => {
         }
 
     })
+
 })
 
 
