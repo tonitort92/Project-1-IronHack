98fe6aebb3ea9945a5abc1960a1575a04755100f
Toni <toni.tort92@gmail.com>
2024-04-25 16:07:10 +0200

    DAY 4 | Finished Project  | Prettier transformation, comments, readMe, little changes

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
 # Project-1-IronHack
-First Project | Toni | IronHack Front-end Bootcamp 
+Mid Term Project
+
+Nombre del Proyecto: Circle Agency | IronHack Front-end Bootcamp 
+
+In this folder you will find the main web assets of Circle website project
+
+
+
+
+
+------------------ | HTML |------------------------------------------------
+
+index.html: main file of the Circle website. Used to load the homepage and it's the default page that
+the browser will open by default. It includes some sections to briefly introduce the brand. 
+
+contact.html: this is the contact page where clients and users can interact with a form to submit 
+their own inquiries and questions. It includes a form with several input fields
+
+projects.html: in this html file you can find the main projects done by Circle, where we present the
+most important ones
+
+
+***
+Main IDs of sections: #menu, #hamburguer-menu-open, #hamburguer-menu-closed, #home-content, #home-cta, #clients-bar, 
+                      #recent-projects, #recent-projects-grid, #testimonial-section, #our-services, #contac-section,
+                      #contact-page, #contact-form, #suscription-box
+
+Other important tags: header, nav, nav ul, footer, .main-button, .project-card, .service-card
+***
+
+
+
+------------------ | CSS |------------------------------------------------
+
+styles.css: we only have one stylesheet for this project with several media queries for 4 possible sizes. Those sizes
+            are based on the best way to distribute the content rather than most common devices. 
+
+
+***
+Main Media Queries: (max-width:599px) --small devices, (min-width:600) and (max-width: 859px) --small devices with big screen or small tablets, 
+                    (min-width:860) and (max-width: 1199px) --tablets or small laptops, (min-width:1200) --large devices
+***
+
+
+
+------------------ | JS |------------------------------------------------
+
+functions.js: this file includes the main features that the webpage will have. The main functionalities are the following. 
+
+***
+Toogle Function: using #hamburger-menu-open and #hamburger-menu-closed we use .addEventListener type 'click' with arrow function to open and close
+                 the hamburger menu when we are working in phone devices. Another .addEventListener type 'resize' supports the function by closing
+                 and hiding the menu if we reescale to large device design.
+
+Fetch of data: using the a Promise, through fetch method and blocks of try and catch, we create inside this block a function called retrieveBackData()
+               to effectively serve images and text inside the website with no hassle. 
+***
+
+
+
+
+
+------------------ | INSTALLATION GUIDE |------------------------------------------------
+
+1. Install Git and GitBash
+2. Fork and Clone this repo from https://github.com/tonitort92/Project-1-IronHack in your computer
+3. Open the project with VSCode or any preferred tool
+4. If you have an host, server or virtual machine set up, you can push this project to your system. 
+5. You might want to use platforms like Netlify if you want to test the project but you have not your own server.
+
+
+
+
+
+TOOLS USED: HTML, CSS, JS, Visual Studio Code
+LICENSE: FREE TO USE FOR ANY PURPOSE 
+CONTACT: toni.tort92@gmail.com
+
+
+
+- Enjoy and have a good day! 
--- scripts/functions.js
+++ scripts/functions.js
@@ -1,120 +1,132 @@
+//We start by fetching our data from our server//
+
 async function retrieveBackData() {
-    try {
-        console.log("Fetching data...");
-        const response = await fetch("https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects");
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const backData = await response.json(); // Convertir la respuesta a JSON aquí
-        console.log(backData);
-        if (backData) {
-            let vectorifyProject = backData[1];
-            let dashcoinProject = backData[2];
-            let simplifyProject = backData[3];
-
-            document.querySelector('#recent-projects h3').textContent = simplifyProject.name;
-            document.querySelector('#recent-projects p').textContent = simplifyProject.description;
-            document.querySelector('.project-card div:nth-of-type(1)').style.backgroundImage = 'url(' + simplifyProject.image + ')';
-        
-            document.querySelectorAll('#recent-projects h3')[1].textContent = dashcoinProject.name;
-            document.querySelectorAll('#recent-projects p')[1].textContent = dashcoinProject.description;
-            document.querySelectorAll('.project-card div:nth-of-type(1)')[1].style.backgroundImage = 'url(' + dashcoinProject.image + ')';
-        
-            document.querySelectorAll('#recent-projects h3')[2].textContent = vectorifyProject.name;
-            document.querySelectorAll('#recent-projects p')[2].textContent = vectorifyProject.description;
-            document.querySelectorAll('.project-card div:nth-of-type(1)')[2].style.backgroundImage = 'url(' + vectorifyProject.image + ')';
-        
-            document.querySelector("#main-project h1").textContent = backData[1].name;
-            document.querySelector("#main-project .date p").textContent = backData[1].description;
-            document.querySelector("#main-project .date span").textContent = backData[1].completed_on;
-            document.querySelector("#main-project .img-project").setAttribute("src", backData[0].image);
-            document.querySelector("#main-project .text-project p").textContent = backData[1].content;
-
-        }
-    } catch (error) {
-        console.log("Something went wrong!", error.message);
+  try {
+    console.log("Fetching data...");
+    const response = await fetch(
+      "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects"
+    );
+    //Throwing error if response is bad//
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
+    //If Data is good, we change some tags and content by our new info//
+    const backData = await response.json();
+    console.log(backData);
+    if (backData) {
+      let vectorifyProject = backData[1];
+      let dashcoinProject = backData[2];
+      let simplifyProject = backData[3];
+      //After storing the data, we start modifications//
+      document.querySelector("#recent-projects h3").textContent =
+        simplifyProject.name;
+      document.querySelector("#recent-projects p").textContent =
+        simplifyProject.description;
+      document.querySelector(
+        ".project-card div:nth-of-type(1)"
+      ).style.backgroundImage = "url(" + simplifyProject.image + ")";
+
+      document.querySelectorAll("#recent-projects h3")[1].textContent =
+        dashcoinProject.name;
+      document.querySelectorAll("#recent-projects p")[1].textContent =
+        dashcoinProject.description;
+      document.querySelectorAll(
+        ".project-card div:nth-of-type(1)"
+      )[1].style.backgroundImage = "url(" + dashcoinProject.image + ")";
+
+      document.querySelectorAll("#recent-projects h3")[2].textContent =
+        vectorifyProject.name;
+      document.querySelectorAll("#recent-projects p")[2].textContent =
+        vectorifyProject.description;
+      document.querySelectorAll(
+        ".project-card div:nth-of-type(1)"
+      )[2].style.backgroundImage = "url(" + vectorifyProject.image + ")";
+
+      document.querySelector("#main-project h1").textContent = backData[1].name;
+      document.querySelector("#main-project .date p").textContent =
+        backData[1].description;
+      document.querySelector("#main-project .date span").textContent =
+        backData[1].completed_on;
+      document
+        .querySelector("#main-project .img-project")
+        .setAttribute("src", backData[0].image);
+      document.querySelector("#main-project .text-project p").textContent =
+        backData[1].content;
+    }
+  } catch (error) {
+    console.log("Something went wrong!", error.message);
+  }
 }
+//We call the function to fetch data here//
 
 retrieveBackData();
 
-/*function projectsSubstitution (a, b, c, d){
 
-    document.querySelectorAll('#recent-projects-grid .project-card div h3')[0].textContent = b.
+window.addEventListener("load", () => {
+  //We want to fix a bug with the mobile menu, as it keeps open in desktop if we open it in mobile //
 
-}
+  //Firstly, we will set up our main queries for the mobile and desktop sizes //
 
-projectsSubstitution( mainProject, vectorifyProject, dashcoinProject, simplifyProject);*/
+  var dekstopSize = window.matchMedia("(min-width:860px)");
+  var mobileSize = window.matchMedia("(max-width:859px)");
 
+  //Here we implement the toogle functionality ONLY if we are in mobile //
 
-window.addEventListener('load', () => {
-
-    //We want to fix a bug with the mobile menu, as it keeps open in desktop if we open it in mobile //
-
-    //Firstly, we will set up our main queries for the mobile and desktop sizes //
-
-    var dekstopSize = window.matchMedia("(min-width:860px)");
-    var mobileSize = window.matchMedia("(max-width:859px)");
-
-    //Here we implement the toogle functionality ONLY if we are in mobile //
-
-    document.getElementById('hamburguer-menu-closed').addEventListener('click', () => {
-
-        if(mobileSize.matches){
-
-            document.getElementById('hamburguer-menu-closed').style.display = 'none';
-            document.getElementById('hamburguer-menu-open').style.display = 'flex';
-        }
+  document
+    .getElementById("hamburguer-menu-closed")
+    .addEventListener("click", () => {
+      if (mobileSize.matches) {
+        document.getElementById("hamburguer-menu-closed").style.display =
+          "none";
+        document.getElementById("hamburguer-menu-open").style.display = "flex";
+      }
     });
 
-    document.getElementById('hamburguer-menu-open').addEventListener('click', () => {
-
-        if(mobileSize.matches){
-
-            document.getElementById('hamburguer-menu-closed').style.display = 'flex';
-            document.getElementById('hamburguer-menu-open').style.display = 'none';
-        }
+  document
+    .getElementById("hamburguer-menu-open")
+    .addEventListener("click", () => {
+      if (mobileSize.matches) {
+        document.getElementById("hamburguer-menu-closed").style.display =
+          "flex";
+        document.getElementById("hamburguer-menu-open").style.display = "none";
+      }
     });
 
-    //Validating the form//
+  document
+    .querySelector("#contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault(); 
 
-    document.querySelector('#contact-form').addEventListener('submit', function(event) {
-        event.preventDefault(); // Esto evita que el formulario se envíe automáticamente.
-    
-        let mainName = document.querySelector('input[type=text]').value;
-        let mail = document.querySelector('input[type=email]').value;
-        let phone = document.querySelector('input[type=tel]').value;
-        let message = document.querySelector('textarea').value; 
-    
-        if(mainName.length == 0 || mail.length == 0 || phone.length == 0 || message.length == 0){
-            alert('Formulario incompleto, vacío o incorrecto');
-        }
-        else{
-            alert('Se ha mandado tu mensaje :D');
-            // Aquí puedes agregar código para enviar los datos del formulario de manera programática si así lo deseas.
-        }
+      let mainName = document.querySelector("input[type=text]").value;
+      let mail = document.querySelector("input[type=email]").value;
+      let phone = document.querySelector("input[type=tel]").value;
+      let message = document.querySelector("textarea").value;
+
+      if (
+        mainName.length == 0 ||
+        mail.length == 0 ||
+        phone.length == 0 ||
+        message.length == 0
+      ) {
+        alert("Formulario incompleto, vacío o incorrecto");
+      } else {
+        alert("Se ha mandado tu mensaje :D");
+      }
     });
 
+  //Now, if you open the toogle, but you resize to desktop, we hide it. If you go back to mobile, we show it again //
 
-    //Now, if you open the toogle, but you resize to desktop, we hide it. If you go back to mobile, we show it again //
+  window.addEventListener("resize", () => {
+    if (dekstopSize.matches) {
+      document.getElementById("hamburguer-menu-closed").style.display = "none";
+      document.getElementById("hamburguer-menu-open").style.display = "none";
+    }
 
-    window.addEventListener('resize', () => {
-
-        if(dekstopSize.matches){
-
-            document.getElementById('hamburguer-menu-closed').style.display = 'none';
-            document.getElementById('hamburguer-menu-open').style.display = 'none';
-
-        }
-
-        if(mobileSize.matches && document.getElementById('hamburguer-menu-open').style.display == 'none'  ){
-
-            document.getElementById('hamburguer-menu-closed').style.display = 'flex';
-
-        }
-
-    })
-
-})
-
-
+    if (
+      mobileSize.matches &&
+      document.getElementById("hamburguer-menu-open").style.display == "none"
+    ) {
+      document.getElementById("hamburguer-menu-closed").style.display = "flex";
+    }
+  });
+});
